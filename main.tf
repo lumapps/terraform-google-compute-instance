85de38f609e1aacaf5e86d08fec4b4b842dac914
Amaury Gauthier <amaury@lumapps.com>
2018-10-18 09:33:14 +0200

    Make gce deployment more modular
    Allow specifying:
      - a GCE zone: force all instances to be in the same zone,
      - a tag (limited to one tag because [Terraform issue
        #17837](https://github.com/hashicorp/terraform/issues/17837) prevent
        us to pass a variable of type list to a module if the list is
        built from other variable interpolation,
      - a network to use: use a GCE network other than "default",
      - data disk: attach a data disk in addition to the boot disk to each
        instances.

--- main.tf
+++ main.tf
@@ -3,20 +3,52 @@ data "google_compute_zones" "available" {
   status = "UP"
 }
 
-resource "google_compute_address" "instances" {
-  count = "${var.amount}"
-  name  = "${var.name_prefix}-${count.index}"
+resource "google_compute_address" "static-addresses" {
+  count  = "${var.amount}"
+  name   = "${var.name_prefix}-${count.index}"
+  region = "${var.region}"
 }
 
-resource "google_compute_disk" "instances" {
+resource "google_compute_disk" "boot-disks" {
   count = "${var.amount}"
 
   name = "${var.name_prefix}-${count.index+1}"
-  type = "${var.disk_type}"
-  size = "${var.disk_size}"
-  zone = "${data.google_compute_zones.available.names[count.index % length(data.google_compute_zones.available.names)]}"
+  type = "${var.boot_disk_type}"
+  size = "${var.boot_disk_size_gb}"
+  zone = "${coalesce(var.zone, data.google_compute_zones.available.names[count.index % length(data.google_compute_zones.available.names)])}"
 
-  image = "${var.disk_image}"
+  image = "${var.boot_disk_image}"
+
+  provisioner "local-exec" {
+    command    = "${var.disk_create_local_exec_command_or_fail}"
+    on_failure = "fail"
+  }
+
+  provisioner "local-exec" {
+    command    = "${var.disk_create_local_exec_command_and_continue}"
+    on_failure = "continue"
+  }
+
+  provisioner "local-exec" {
+    when       = "destroy"
+    command    = "${var.disk_destroy_local_exec_command_or_fail}"
+    on_failure = "fail"
+  }
+
+  provisioner "local-exec" {
+    when       = "destroy"
+    command    = "${var.disk_destroy_local_exec_command_and_continue}"
+    on_failure = "continue"
+  }
+}
+
+resource "google_compute_disk" "data-disks" {
+  count = "${var.amount}"
+
+  name = "${var.name_prefix}-data-${count.index+1}"
+  type = "${var.data_disk_type}"
+  size = "${var.data_disk_size_gb}"
+  zone = "${coalesce(var.zone, data.google_compute_zones.available.names[count.index % length(data.google_compute_zones.available.names)])}"
 
   provisioner "local-exec" {
     command    = "${var.disk_create_local_exec_command_or_fail}"
@@ -44,27 +76,30 @@ resource "google_compute_disk" "instances" {
 resource "google_compute_instance" "instances" {
   count = "${var.amount}"
 
-  name         = "${var.name_prefix}-${count.index+1}"
-  zone         = "${data.google_compute_zones.available.names[count.index % length(data.google_compute_zones.available.names)]}"
+  name = "${var.name_prefix}-${count.index+1}"
+  zone = "${coalesce(var.zone, data.google_compute_zones.available.names[count.index % length(data.google_compute_zones.available.names)])}"
+
   machine_type = "${var.machine_type}"
+  tags         = ["${var.tag}"]
 
   boot_disk = {
-    source      = "${google_compute_disk.instances.*.name[count.index]}"
+    source      = "${google_compute_disk.boot-disks.*.name[count.index]}"
     auto_delete = false
   }
 
-  metadata {
-    user-data = "${replace(replace(var.user_data, "$$ZONE", data.google_compute_zones.available.names[count.index]), "$$REGION", var.region)}"
+  attached_disk = {
+    source = "${google_compute_disk.data-disks.*.name[count.index]}"
   }
+
+  //TODO: service account.
 
   network_interface = {
-    network = "default"
+    network = "${var.network}"
 
     access_config = {
-      nat_ip = "${google_compute_address.instances.*.address[count.index]}"
+      nat_ip = "${google_compute_address.static-addresses.*.address[count.index]}"
     }
   }
-
   scheduling {
     on_host_maintenance = "MIGRATE"
     automatic_restart   = "true"
--- variables.tf
+++ variables.tf
@@ -1,16 +1,34 @@
 variable "region" {}
 
+variable "zone" {
+  default = ""
+}
+
 variable "amount" {}
 variable "name_prefix" {}
 variable "machine_type" {}
-variable "user_data" {}
 
-variable "disk_type" {
+variable "tag" {
+  type    = "string"
+  default = ""
+}
+
+variable "network" {
+  default = "default"
+}
+
+variable "boot_disk_type" {
+  default = "pd-standard"
+}
+
+variable "boot_disk_size_gb" {}
+variable "boot_disk_image" {}
+
+variable "data_disk_type" {
   default = "pd-ssd"
 }
 
-variable "disk_size" {}
-variable "disk_image" {}
+variable "data_disk_size_gb" {}
 
 variable "disk_create_local_exec_command_or_fail" {
   default = ":"
